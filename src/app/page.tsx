fd12c606147fa17dd04620299470cd09ed0e07d3
류정우 <evencoding@gmail.com>
2024-08-10 11:30:21 +0900

    feat: getGoldenTime

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Icon from '@/components/icon'
-
 export default function Home() {
-  return (
-    <div>
-      <Icon name="checkbox-off" onClick={() => {}} />
-    </div>
-  )
+  return <div></div>
 }
---
+++ src/utils/date.ts
@@ -0,0 +1,31 @@
+export function getGoldenTime(
+  date: Date,
+  options: {
+    fullDeploy: boolean
+  } = {
+    fullDeploy: false,
+  }
+) {
+  const now = new Date()
+  const diff = date.getTime() - now.getTime()
+
+  const msHour = 3600 * 1000
+  const msMin = 60 * 1000
+  const msSecond = 1000
+
+  const hours = Math.floor(diff / msHour)
+  const minutes = Math.floor(((diff % msHour) / 60) * msSecond)
+  const seconds = Math.floor((diff % msMin) / msSecond)
+
+  if (options.fullDeploy) {
+    return `${hours}시간 ${minutes}분 ${seconds}초`
+  } else {
+    if (hours > 0) {
+      return `${hours}시간`
+    } else if (minutes > 0) {
+      return `${minutes}분`
+    } else {
+      return `${seconds}초`
+    }
+  }
+}
